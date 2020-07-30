6e783639bd3954b1696ee1b9cefe35489c385c0a
Marena Linda <marena25@protonmail.com>
2020-07-29 20:17:27 -0400

    should fix kenny and konstantin

---
+++ build/precache-manifest.4bc96e12976fda25445bd5cd5fc1b216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bcaa73db0494939727d0c5818cc89a",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "8a407a9c7d473c5df23a",
+    "url": "/memory-game/static/css/main.4d1e26ab.chunk.css"
+  },
+  {
+    "revision": "2f5f36369ad9babcf516",
+    "url": "/memory-game/static/js/2.d096ea45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.d096ea45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a407a9c7d473c5df23a",
+    "url": "/memory-game/static/js/main.9da45dc4.chunk.js"
+  },
+  {
+    "revision": "4e1481c7ed3ba6ba1a2e",
+    "url": "/memory-game/static/js/runtime-main.983dcba8.js"
+  }
+]);
