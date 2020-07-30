d2f34ec1ccc7668cd51a344ead6214a371cafca1
Marena Linda <marena25@protonmail.com>
2020-07-29 21:19:37 -0400

    Updates

---
+++ precache-manifest.62b12d9737cf5521a21b9313a912704e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36127a212081f83c89b67c2582e2ffe5",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "5cd5fa4b07cd6116ac2a",
+    "url": "/memory-game/static/css/main.4d1e26ab.chunk.css"
+  },
+  {
+    "revision": "689649a47605e069e48a",
+    "url": "/memory-game/static/js/2.a3fab9b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.a3fab9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd5fa4b07cd6116ac2a",
+    "url": "/memory-game/static/js/main.c7907c71.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
